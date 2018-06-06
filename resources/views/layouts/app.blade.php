163fc1d0e440e3a2ed464695bad6e9a34bf378c7
AnthonyS <anthonydsaviano@gmail.com>
2018-06-06 16:38:24 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap core CSS -->
     <link href="{{asset('sass/app.scss')}}" rel="stylesheet">
     <link href="{{asset('css/custom.css')}}" rel="stylesheet">
-
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 
   <body>
