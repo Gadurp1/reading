aad1bcd4c3cea6ee9099ab51ca40be9a42edf035
AnthonyS <anthonydsaviano@gmail.com>
2018-06-06 16:33:29 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>Reading List</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{asset('css/custom.css')}}" rel="stylesheet">
 
 </head>
